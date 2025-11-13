0b4048865e3bc2e1fe97d2f695761d7ba3c3122a
lym <lym>
2025-11-13 16:17:45 +0800

    update for test deepseek 阻力位分析

---
+++ t1/kline_ext.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"github.com/adshao/go-binance/v2/futures"
+	"github.com/markcheno/go-talib"
+	"reflect"
+	"strconv"
+)
+
+type KlineExt struct {
+	futures.Kline
+	EMA5       float64
+	EMA20      float64
+	EMA60      float64
+	Macd       float64
+	MacdSignal float64
+	MacdHist   float64
+}
+
+func FromRawKline(rawKline *futures.Kline) *KlineExt {
+	return &KlineExt{
+		Kline: *rawKline,
+	}
+}
+
+func FromRawKlines(rawKlines []*futures.Kline) KLinesExtSlice {
+	klines := make([]*KlineExt, 0)
+	for _, rawKline := range rawKlines {
+		klines = append(klines, FromRawKline(rawKline))
+	}
+	return klines
+}
+
+type KLinesExtSlice []*KlineExt
+
+func (kes *KLinesExtSlice) ClosePrices() []float64 {
+	return kes.FieldSlice("Close")
+}
+
+func (kes *KLinesExtSlice) OpenPrices() []float64 {
+	return kes.FieldSlice("Open")
+}
+
+func (kes *KLinesExtSlice) FieldSlice(filedName string) []float64 {
+	// 通过反射获取指定字段的值,如果类型不是float64则转换
+	values := make([]float64, 0)
+	for _, kline := range *kes {
+		value := reflect.ValueOf(kline).Elem().FieldByName(filedName)
+		if value.IsValid() {
+			if value.Kind() == reflect.Float64 {
+				values = append(values, value.Float())
+			} else if value.Kind() == reflect.String {
+				v, _ := strconv.ParseFloat(value.String(), 64)
+				values = append(values, v)
+			}
+		}
+	}
+	return values
+}
+
+func (kes *KLinesExtSlice) setFieldSlice(filedName string, data []float64) {
+	for i, kline := range *kes {
+		value := reflect.ValueOf(kline).Elem().FieldByName(filedName)
+		if value.IsValid() && value.CanSet() && value.Kind() == reflect.Float64 {
+			value.SetFloat(data[i])
+		}
+	}
+}
+
+func (kes *KLinesExtSlice) calcEma(period int) {
+	values := talib.Ema(kes.ClosePrices(), period)
+	fieldName := "EMA" + strconv.Itoa(period)
+	kes.setFieldSlice(fieldName, values)
+}
+
+func (kes *KLinesExtSlice) calcMacd() {
+	macd, signal, hist := talib.Macd(kes.ClosePrices(), 12, 26, 9)
+	kes.setFieldSlice("Macd", macd)
+	kes.setFieldSlice("MacdSignal", signal)
+	kes.setFieldSlice("MacdHist", hist)
+}
---
+++ t1/t1.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/adshao/go-binance/v2"
+	"time"
+)
+
+var (
+	apiKey    = "hWB2yqoNw4xTOU0uKSPlVZJfILgCzSLEK82cYsPmZdln06RKY09QpufiisR0P77C"
+	secretKey = "0fAVqa4F8ilsxO00lpkD43vX4XjKEZ4WehgX2wkRdyDz3ZSXBncNkoyrtB0HwTSd"
+
+	deepseekApiKey = "sk-e47daa8c2a46411b936857bf4e57f755"
+)
+
+func main() {
+
+	//httpClient := http.Client{
+	//	Timeout: 10 * 1e9,
+	//}
+	futuresClient := binance.NewFuturesClient(apiKey, secretKey) // USDT-M Futures
+	klines, err := futuresClient.NewKlinesService().
+		Interval("4h").
+		StartTime(time.Now().Add(-10 * 24 * time.Hour).UnixMilli()).
+		Symbol("BTCUSDT").Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	klinesExt := FromRawKlines(klines)
+	klinesExt.calcEma(5)
+	klinesExt.calcEma(20)
+	klinesExt.calcEma(60)
+	klinesExt.calcMacd()
+
+	data, _ := json.Marshal(klinesExt)
+	println(string(data))
+}
